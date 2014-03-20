cc516aecb20017493c1617739fe7dfb015cfd839
jayeshm3 <jayesh.mair@citiustech.com>
2014-03-20 18:19:47 +0530

    ++ Adding user query based on email only

--- ipf-services/src/main/java/com/iplfreaks/dao/repository/UserRepository.java
+++ ipf-services/src/main/java/com/iplfreaks/dao/repository/UserRepository.java
@@ -6,6 +6,7 @@ package com.iplfreaks.dao.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.iplfreaks.user.User;
 
@@ -16,5 +17,8 @@ import com.iplfreaks.user.User;
 public interface UserRepository extends MongoRepository<User, String> {
 	
 	List<User> findByEmailAndPassword(final String email, final String password);
+	
+	@Query(fields = "{'email':1,'password':0}")
+	List<User> findByEmail(final String email);
 
 }
