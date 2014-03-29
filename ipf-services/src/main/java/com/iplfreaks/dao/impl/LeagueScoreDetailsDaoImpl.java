a396fb824146775430d1a53dc3123289f82646b2
asdivekar <aniket.divekar@gmail.com>
2014-03-29 22:08:40 +0530

    removed unwanted @Autowired annotation

--- ipf-services/src/main/java/com/iplfreaks/dao/impl/LeagueScoreDetailsDaoImpl.java
+++ ipf-services/src/main/java/com/iplfreaks/dao/impl/LeagueScoreDetailsDaoImpl.java
@@ -6,7 +6,6 @@ package com.iplfreaks.dao.impl;
 import java.util.List;
 import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -21,76 +20,82 @@ import com.mongodb.WriteResult;
 
 /**
  * @author jayeshm3
- *
+ * 
  */
 public class LeagueScoreDetailsDaoImpl implements ILeagueScoreDetailsDao {
-	
-	@Autowired
+
 	private MongoTemplate mongoTemplate;
-	
-	@Autowired
+
 	private LeagueScoreDetailsRepository leagueScoreDetailsRepository;
-	
 
 	@Override
-	public void createNewLeagueScoreDetails(LeagueScoreDetails leagueScoreDetails) {
+	public void createNewLeagueScoreDetails(
+			LeagueScoreDetails leagueScoreDetails) {
 		this.mongoTemplate.insert(leagueScoreDetails);
 	}
 
 	@Override
 	public void createNewLeagueScoreDetails(String leagueName) {
-		final LeagueScoreDetails leagueScoreDetails = new LeagueScoreDetails(leagueName);
+		final LeagueScoreDetails leagueScoreDetails = new LeagueScoreDetails(
+				leagueName);
 		this.mongoTemplate.insert(leagueScoreDetails);
 	}
 
 	@Override
 	public void createNewLeagueScoreDetails(String leagueName,
 			Set<Challenge> challenges) {
-		
-		final LeagueScoreDetails leagueScoreDetails = new LeagueScoreDetails(leagueName);
+
+		final LeagueScoreDetails leagueScoreDetails = new LeagueScoreDetails(
+				leagueName);
 		leagueScoreDetails.addAllFixturePrediction(challenges);
 		this.mongoTemplate.insert(leagueScoreDetails);
 	}
-	
 
 	@Override
 	public LeagueScoreDetails fetchUserChallenge(String leagueName,
 			String fixtureId, String email) {
-		final List<LeagueScoreDetails> leagueScoreDetails = getLeagueScoreDetailsRepository().findByleagueNameAndChallengesFixtureIdAndChallengesPredictionsChallengerUserEmail(leagueName, fixtureId, email);
+		final List<LeagueScoreDetails> leagueScoreDetails = getLeagueScoreDetailsRepository()
+				.findByleagueNameAndChallengesFixtureIdAndChallengesPredictionsChallengerUserEmail(
+						leagueName, fixtureId, email);
 
-		if(leagueScoreDetails == null || leagueScoreDetails.isEmpty())
-		{
+		if (leagueScoreDetails == null || leagueScoreDetails.isEmpty()) {
 			return null;
 		}
-		
+
 		return leagueScoreDetails.get(0);
 	}
 
 	@Override
 	public void addChallengerPrediction(String leagueName, String fixtureId,
 			Prediction prediction) {
-		
-		//final LeagueScoreDetails leagueScoreDetails = fetchLeagueScoreDetails(leagueName);
-		
-		final Query query = new Query(Criteria.where("leagueName").is(leagueName).andOperator(Criteria.where("challenges.fixtureId").is(fixtureId)));
+
+		// final LeagueScoreDetails leagueScoreDetails =
+		// fetchLeagueScoreDetails(leagueName);
+
+		final Query query = new Query(Criteria
+				.where("leagueName")
+				.is(leagueName)
+				.andOperator(
+						Criteria.where("challenges.fixtureId").is(fixtureId)));
 		final Update update = new Update();
 		update.addToSet("challenges.$.predictions", prediction);
-		//this.mongoTemplate.upsert(query, update, "leagueScoreDetails");
-		final WriteResult writeResult = this.mongoTemplate.updateFirst(query, update, "leagueScoreDetails");
+		// this.mongoTemplate.upsert(query, update, "leagueScoreDetails");
+		final WriteResult writeResult = this.mongoTemplate.updateFirst(query,
+				update, "leagueScoreDetails");
 		System.out.println(writeResult.toString());
 		System.out.println(writeResult.getLastError());
-		
+
 	}
 
 	@Override
 	public LeagueScoreDetails fetchLeagueScoreDetails(String leagueName) {
-		final List<LeagueScoreDetails> leagueScoreDetails = getLeagueScoreDetailsRepository().findByleagueName(leagueName);
+		final List<LeagueScoreDetails> leagueScoreDetails = getLeagueScoreDetailsRepository()
+				.findByleagueName(leagueName);
 
-		if(leagueScoreDetails == null || leagueScoreDetails.isEmpty())
-		{
+		if (leagueScoreDetails == null || leagueScoreDetails.isEmpty()) {
 			return null;
 		}
-		
+
 		return leagueScoreDetails.get(0);
 	}
 
@@ -102,7 +107,8 @@ public class LeagueScoreDetailsDaoImpl implements ILeagueScoreDetailsDao {
 	}
 
 	/**
-	 * @param mongoTemplate the mongoTemplate to set
+	 * @param mongoTemplate
+	 *            the mongoTemplate to set
 	 */
 	public void setMongoTemplate(MongoTemplate mongoTemplate) {
 		this.mongoTemplate = mongoTemplate;
@@ -116,7 +122,8 @@ public class LeagueScoreDetailsDaoImpl implements ILeagueScoreDetailsDao {
 	}
 
 	/**
-	 * @param leagueScoreDetailsRepository the leagueScoreDetailsRepository to set
+	 * @param leagueScoreDetailsRepository
+	 *            the leagueScoreDetailsRepository to set
 	 */
 	public void setLeagueScoreDetailsRepository(
 			LeagueScoreDetailsRepository leagueScoreDetailsRepository) {
