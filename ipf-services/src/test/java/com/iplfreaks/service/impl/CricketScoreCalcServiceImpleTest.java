c9285a69dbab89b4b1dba801f21ebc5102d7f86c
jayeshm3 <jayesh.mair@citiustech.com>
2014-04-04 16:55:42 +0530

    ++ Adding score testing

--- ipf-services/src/test/java/com/iplfreaks/service/impl/CricketScoreCalcServiceImpleTest.java
+++ ipf-services/src/test/java/com/iplfreaks/service/impl/CricketScoreCalcServiceImpleTest.java
@@ -12,12 +12,14 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.iplfreaks.core.Challenger;
-import com.iplfreaks.core.Competition;
-import com.iplfreaks.core.League;
-import com.iplfreaks.dao.impl.CricketLeagueScoreDaoImpl;
+import com.iplfreaks.core.BonusEntity;
 import com.iplfreaks.dao.impl.CricketLeagueDaoImpl;
+import com.iplfreaks.dao.impl.CricketLeagueScoreDaoImpl;
 import com.iplfreaks.game.cricket.CricketChallenge;
+import com.iplfreaks.game.cricket.CricketCompetition;
+import com.iplfreaks.game.cricket.CricketFixture;
+import com.iplfreaks.game.cricket.CricketFixtureOutcome;
+import com.iplfreaks.game.cricket.CricketLeague;
 import com.iplfreaks.game.cricket.CricketLeagueScore;
 import com.iplfreaks.game.cricket.CricketPrediction;
 
@@ -40,7 +42,7 @@ public class CricketScoreCalcServiceImpleTest {
 	
 	@Test
 	public void testFetchLeague() {
-		final League league = leagueDaoImpl.fetchLeague("iplfreaks");
+		final CricketLeague league = leagueDaoImpl.fetchLeague("iplfreaks");
 		System.out.println(league.getLeagueStartDate());
 		System.out.println("Challenger count : " + league.getChallengers().size());
 		System.out.println("Pending Challenger count : " + league.getPendingChallengers().size());
@@ -49,7 +51,7 @@ public class CricketScoreCalcServiceImpleTest {
 	@Test
 	public void testFetchCricketLeagueScore()
 	{
-		final CricketLeagueScore leagueScoreDetails = null;
+		final CricketLeagueScore leagueScoreDetails = cricketLeagueScoreDaoImpl.fetchCricketLeagueScore("iplfreaks");
 		
 		final Set<CricketChallenge> challenges = leagueScoreDetails.getFixturePredictionList();
 		System.out.println("Challenges size : " + challenges.size());
@@ -71,11 +73,96 @@ public class CricketScoreCalcServiceImpleTest {
 		}
 	}
 	
-	private void testCricketScoreCalc(final League league, final CricketLeagueScore cricketLeagueScore)
+	@Test
+	public void testCalc()
+	{
+		final String leagueName = "iplfreaks";
+		final CricketLeague league = leagueDaoImpl.fetchLeague(leagueName);
+		final CricketLeagueScore cricketLeagueScore = cricketLeagueScoreDaoImpl.fetchCricketLeagueScore(leagueName);
+		testCricketScoreCalc(league, cricketLeagueScore);
+	}
+	
+	private void testCricketScoreCalc(final CricketLeague league, final CricketLeagueScore cricketLeagueScore)
 	{
 		//final Set<Challenger> challengers = league.getChallengers();
+		final CricketCompetition competition = league.getCompetition();
+		
+		final Set<CricketChallenge> cricketChallenges = cricketLeagueScore.getFixturePredictionList();
+		for(final CricketChallenge cricketChallenge : cricketChallenges)
+		{
+			final CricketFixtureOutcome outcome = getCricketFixture(competition, cricketChallenge.getFixtureId());
+			getScore(outcome, cricketChallenge);
+		}
+		
+	}
+	
+	private CricketFixtureOutcome getCricketFixture(final CricketCompetition competition, final String fixtureId)
+	{
+		for(final CricketFixture cf : competition.getFixtures())
+		{
+			if(cf.getFixtureId().equals(fixtureId))
+			{
+				return cf.getOutcome();
+			}
+		}
+		
+		return null;
+	}
+	
+	private int getScore(final CricketFixtureOutcome outcome, final CricketChallenge cricketChallenge )
+	{
+		for(final CricketPrediction cricketPrediction : cricketChallenge.getPredictions())
+		{
+			System.out.println(cricketPrediction.getChallenger().getUser().getEmail() + " Prediction score :");
+			if(outcome.getBestBatsman().equals(cricketPrediction.getBestBatsman()))
+			{
+				System.out.println("Batsman prediction : Hit" );
+			}
+			else{
+				System.out.println("Batsman prediction : Miss" );
+			}
+			
+			if(outcome.getBestBowler().equals(cricketPrediction.getBestBowler()))
+			{
+				System.out.println("Bowler prediction : Hit" );
+			}
+			else{
+				System.out.println("Bowler prediction : Miss" );
+			}
+			
+			if(outcome.getManOfTheMatch().equals(cricketPrediction.getManOfTheMatch()))
+			{
+				System.out.println("Man of the match prediction : Hit" );
+			}
+			else{
+				System.out.println("Man of the match prediction : Miss" );
+			}
+			
+			if(outcome.getWinner().equals(cricketPrediction.getWinnerTeam()))
+			{
+				System.out.println("Winner team prediction : Hit" );
+			}
+			else{
+				System.out.println("Winner team prediction : Miss" );
+			}
+			
+			int i = 0;
+			for(final BonusEntity be : outcome.getBonus())
+			{
+				if(be.getBonusAnswer().equals(cricketPrediction.getBonus().get(i)))
+				{
+					System.out.println("Bonus " + be.getBonusQuestion() + " prediction : Hit" );
+				}
+				else{
+					System.out.println("Bonus " + be.getBonusQuestion() + " prediction : Miss" );
+				}
+				i++;
+			}
+			
+		}
 		
 		
+		return 0;
 	}
 
 	/**
