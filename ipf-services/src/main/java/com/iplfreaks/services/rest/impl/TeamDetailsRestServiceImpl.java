08848c9ccd36a68bf66057ce877aa1dd2bd88a74
asdivekar <aniket.divekar@gmail.com>
2014-04-06 13:52:19 +0530

    updated to send standard Rest Response

--- ipf-services/src/main/java/com/iplfreaks/services/rest/impl/TeamDetailsRestServiceImpl.java
+++ ipf-services/src/main/java/com/iplfreaks/services/rest/impl/TeamDetailsRestServiceImpl.java
@@ -1,6 +1,12 @@
 package com.iplfreaks.services.rest.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.google.gson.Gson;
+import com.iplfreaks.common.Keys;
+import com.iplfreaks.common.RestServiceResponse;
+import com.iplfreaks.common.Status;
 import com.iplfreaks.game.Team;
 import com.iplfreaks.services.api.ITeamDetailsService;
 import com.iplfreaks.services.rest.api.ITeamDetailsRestService;
@@ -11,8 +17,21 @@ public class TeamDetailsRestServiceImpl implements ITeamDetailsRestService {
 
 	@Override
 	public String getTeamDetails(String teamName) {
-		final Team team = this.teamDetailsService.getTeamDetails(teamName);
-		return new Gson().toJson(team);
+
+		RestServiceResponse response = null;
+		try {
+			Map<String, Object> result = new HashMap<String, Object>();
+			final Team team = this.teamDetailsService.getTeamDetails(teamName);
+			result.put(Keys.TEAM.name(), team);
+			response = new RestServiceResponse(Status.SUCCESS.name(), null,
+					result);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			response = new RestServiceResponse(Status.ERROR.name(),
+					e.getMessage(), null);
+		}
+		return new Gson().toJson(response);
 	}
 
 	public ITeamDetailsService getTeamDetailsService() {
