2973aaecba84fcdebbd46307730729847132266a
hirend <hdeveliya483@gmail.com>
2014-04-09 15:30:13 +0530

    Changes commited

--- ipf-dashboard/app/controllers/Dashboard.java
+++ ipf-dashboard/app/controllers/Dashboard.java
@@ -19,29 +19,76 @@ public class Dashboard extends Controller {
 				queryParams);
 		return ok(response.getBody());
 	}
-	
-	public static Result getFixturesDetails(String leagueName, String type) {
+
+	public static Result getAllFixtures(String leagueName, String type) {
 
 		System.out.println("leagueName " + leagueName);
 		String url = "";
-		if(type.equalsIgnoreCase("past"))
-		{
+		if (type.equalsIgnoreCase("past")) {
 			url = "/services/cricketfixtures/getPastFixtures";
-		}else if(type.equalsIgnoreCase("future"))
-		{
+		} else if (type.equalsIgnoreCase("future")) {
 			url = "/services/cricketfixtures/getUpcomingFixtures";
-		}else if(type.equalsIgnoreCase("present"))
-		{
+		} else if (type.equalsIgnoreCase("present")) {
 			url = "/services/cricketfixtures/getFixtures";
 		}
-		
+
 		Map<String, String> queryParams = new HashMap<String, String>();
 		queryParams.put("leagueName", leagueName);
-		WS.Response response = ServiceUtil.callGET(
-				url, "application/json",
+		WS.Response response = ServiceUtil.callGET(url, "application/json",
 				queryParams);
-		System.out.println("the result of getFixturesDetails "+response.getBody());
+		System.out.println("the result of getFixturesDetails "
+				+ response.getBody());
 		return ok(response.getBody());
 	}
+	
+	public static Result getFixtureDetails(String leagueName, String fixtureId) {
 
+		Map<String, String> queryParams = new HashMap<String, String>();
+		queryParams.put("leagueName", leagueName);
+		queryParams.put("fixtureId", fixtureId);
+		String response = ServiceUtil.callPOST("/services/challergerService/getCricketChallenges/",
+				null, queryParams, 50000);
+		return ok(response);
+
+	}
+	
+	public static Result savePredictions(String fixtureId, String leagueName, String maxWct, String maxRun, String MofM, String mWnr, String bAns) {
+
+		Map<String, String> queryParams = new HashMap<String, String>();
+		queryParams.put("fixtureId", fixtureId);
+		queryParams.put("leagueName", leagueName);
+		queryParams.put("challengerEmailId", session().get("email"));
+		if(maxWct.equalsIgnoreCase("Drag Player here"))
+		{
+			maxWct = "";
+		}
+		if(maxRun.equalsIgnoreCase("Drag Player here"))
+		{
+			maxRun = "";
+		}
+		if(MofM.equalsIgnoreCase("Drag Player here"))
+		{
+			MofM = "";
+		}
+		if(mWnr.equalsIgnoreCase("Drag Team here"))
+		{
+			mWnr = "";
+		}
+		if(bAns.equalsIgnoreCase("Drag Bonus Ans here"))
+		{
+			bAns = "";
+		}
+		queryParams.put("bestBowler", maxWct);
+		queryParams.put("bestBatsman", maxRun);
+		queryParams.put("manOfTheMatch", MofM);
+		queryParams.put("winnerTeam", mWnr);
+		queryParams.put("bonusAnswer", bAns);
+		
+		System.out.println("maxWct "+maxWct +"mWnr "+mWnr + session().get("email"));
+		String response = ServiceUtil.callPOST("/services/prediction/saveCricketPrediction/",
+				null, queryParams, 50000);
+		return ok(response);
+
+	}
+	
 }
