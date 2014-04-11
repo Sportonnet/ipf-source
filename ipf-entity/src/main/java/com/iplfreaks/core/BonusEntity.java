61c11139db72ed51cf46ef581856f55d5fbddf51
jayeshm3 <jayesh.mair@citiustech.com>
2014-04-11 17:12:08 +0530

    ++ Implementing equals and override method

--- ipf-entity/src/main/java/com/iplfreaks/core/BonusEntity.java
+++ ipf-entity/src/main/java/com/iplfreaks/core/BonusEntity.java
@@ -60,4 +60,43 @@ public class BonusEntity {
 		this.options = bonusOptions;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((bonusAnswer == null) ? 0 : bonusAnswer.hashCode());
+		result = prime * result
+				+ ((bonusQuestion == null) ? 0 : bonusQuestion.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BonusEntity other = (BonusEntity) obj;
+		if (bonusAnswer == null) {
+			if (other.bonusAnswer != null)
+				return false;
+		} else if (!bonusAnswer.equals(other.bonusAnswer))
+			return false;
+		if (bonusQuestion == null) {
+			if (other.bonusQuestion != null)
+				return false;
+		} else if (!bonusQuestion.equals(other.bonusQuestion))
+			return false;
+		return true;
+	}
+	
 }
