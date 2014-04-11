6d586db9a5856b13d77062d8df537cf2f4674e43
asdivekar <aniket.divekar@gmail.com>
2014-04-11 13:11:43 +0530

    overridden equals and hashCode methods

--- ipf-entity/src/main/java/com/iplfreaks/game/Player.java
+++ ipf-entity/src/main/java/com/iplfreaks/game/Player.java
@@ -64,9 +64,7 @@ public class Player extends BaseEntity {
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
+				if (getClass() != obj.getClass())
 			return false;
 		Player other = (Player) obj;
 		if (name == null) {
--- ipf-entity/src/main/java/com/iplfreaks/game/Team.java
+++ ipf-entity/src/main/java/com/iplfreaks/game/Team.java
@@ -78,4 +78,33 @@ public class Team extends BaseEntity {
 		this.homeVenue = homeVenue;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (getClass() != obj.getClass())
+			return false;
+		Team other = (Team) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
